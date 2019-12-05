555a5e5b59af7c1cea02e5685a8d120a855e46d1
rakw1995 <54558702+rakw1995@users.noreply.github.com>
2019-12-04 19:56:07 -0500

    more

--- HealthBar2D.gd
+++ HealthBar2D.gd
@@ -1,8 +1,8 @@
 extends TextureProgress
 
-var bar_red = preload("res://assets/barHorizontal_red_mid 200.png")
-var bar_green = preload("res://assets/barHorizontal_green_mid 200.png")
-var bar_yellow = preload("res://assets/barHorizontal_yellow_mid 200.png")
+var bar_red = preload("res://Assets/barHorizontal_red_mid 200.png")
+var bar_green = preload("res://Assets/barHorizontal_green_mid 200.png")
+var bar_yellow = preload("res://Assets/barHorizontal_yellow_mid 200.png")
 				
 #func _ready():
 #	hide()
